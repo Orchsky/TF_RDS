1e71518f61d8379c605fe35f4efcb6315799adf9
starseed777 <waress66@live.com>
2023-01-21 13:14:33 -0500

    current code

---
+++ rds.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias  = "replica"
+}
+
+resource "aws_db_instance" "dbexample" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
+resource "aws_kms_key" "kmsexample" {
+  description = "Encryption key for automated backups"
+}
+
+resource "aws_db_instance_automated_backups_replication" "examplereplication" {
+  source_db_instance_arn = aws_db_instance.dbexample.arn
+  kms_key_id             = aws_kms_key.kmsexample.arn
+}
